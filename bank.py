b1d7a459642b3b6c6aea140a7ae73ce51dfa7be0
jojowkg <k.joldoshbekova@gmail.com>
2018-01-19 17:30:45 +0600

    Final

--- bank.py
+++ bank.py
@@ -1,10 +1,10 @@
 amount = float(input ("What is your deposit amount?  "))
 years = int(input ("For how many years you want to deposit? "))
-i=0
-interest=0
+i = 0
+interest = 0
 for year in range(0, years): 
 	interest = (amount / 100) * 10
-	amount = amount+ interest
+	amount = amount + interest
 	i = i + 1
 
 print("Your deposit in " + str(years) + " years will become "+ str(amount))
